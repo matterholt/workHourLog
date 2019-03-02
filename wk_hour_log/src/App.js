edc3d63bee99d37423154233108b671c598cab8e
MAtterholt <matterholt84@gmail.com>
2019-03-02 09:57:42 -0500

    change to React

---
+++ wk_hour_log/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import HookConvert from './componets/HookConvert';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Weekly Hours</h1>
+        <HookConvert />
+      </div>
+    );
+  }
+}
+
+export default App;
