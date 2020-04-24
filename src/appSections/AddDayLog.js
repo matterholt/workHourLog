f2bc63bc648c1b10665f8b0b5f1500f3c5291c6d
MAtterholt <matterholt84@gmail.com>
2020-04-24 12:59:54 -0400

    updated styles of Add form

--- src/appSections/AddDayLog.js
+++ src/appSections/AddDayLog.js
@@ -6,16 +6,18 @@ import { useIsModalOpen } from "../hooks/useIsModalOpen";
 
 const NewDayDiv = styled.div`
   background: #7c7c86;
-  padding: 2px;
+  border-radius: 0.3em;
+  overflow: hidden;
 `;
 
 /// building Form style!
 const Form = styled.form`
   margin: 10px;
-  width: 400px;
+  max-width: 400px;
   padding: 1em;
   border: 1px solid #ccc;
-  border-radius: 1em;
+  border-radius: inherit;
+  background-color: white;
 `;
 
 const Form_ul = styled.ul`
@@ -26,12 +28,15 @@ const Form_ul = styled.ul`
 
 const Form_li = styled.li`
   margin-top: 1em;
+  display: flex;
+  flex-flow: ${(props) => (props.row ? "row" : "column")};
 `;
 
 const Form_label = styled.label`
   display: inline-block;
   width: 90px;
-  text-align: right;
+  margin-bottom: 0.2em;
+  font-weight: 900;
 `;
 
 const Form_input = styled.input`
@@ -46,12 +51,22 @@ const Form_input = styled.input`
     border-color: #000;
   }
 `;
+
 const Form_textArea = Form_input.withComponent("textarea");
 
 const Form_button = styled.button`
-  margin-left: 90px;
+  margin: 0 10px;
 `;
 
+const Form_heading = styled.h1`
+  margin: 0;
+  text-align: center;
+`;
+
+const Form_text = styled.p`
+  margin: 1em 0 0 0;
+  text-align: center;
+`;
 ////
 const NewDayForm = () => {
   function handSubmit(event) {
@@ -62,6 +77,8 @@ const NewDayForm = () => {
   // lets make it a good form
   return (
     <Form onSubmit={handSubmit}>
+      <Form_heading> Task</Form_heading>
+      <Form_text>Task to get done</Form_text>
       <Form_ul>
         <Form_li>
           <Form_label htmlFor="dateSelect">Select Day:</Form_label>
@@ -72,27 +89,40 @@ const NewDayForm = () => {
           <Form_input id="startTime" name="startTime" type="time" />
         </Form_li>
         <Form_li>
-          <Form_label htmlFor="quitTime">Quit Time:</Form_label>
+          <Form_label htmlFor="quitTime">End Time:</Form_label>
           <Form_input id="quitTime" name="quitTime" type="time" />
         </Form_li>
         <Form_li>
           <Form_label htmlFor="taskNote">Description:</Form_label>
           <Form_textArea textarea id="taskNote" name="taskNote" />
         </Form_li>
-        <Form_li>
+        <Form_li row>
           <Form_button type="submit">Submit</Form_button>
+          <Form_button type="reset">Reset</Form_button>
         </Form_li>
       </Form_ul>
     </Form>
   );
 };
 
+const ModalHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 35px;
+  background-color: #4b4b51;
+  color: white;
+  align-items: center;
+  padding: 0 12px;
+`;
+
 const AddTaskModal = (props) => {
   return (
     <ModelLayout>
       <NewDayDiv>
-        <h1> Add Day</h1>
-        <button onClick={props.updateShown}>close</button>
+        <ModalHeader>
+          <p>Add Task</p>
+          <button onClick={props.updateShown}>close</button>
+        </ModalHeader>
         <NewDayForm />
       </NewDayDiv>
     </ModelLayout>
