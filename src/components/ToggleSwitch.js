d9a9ad73cb69920bcc2965bb0b414c27a735327c
MAtterholt <matterholt84@gmail.com>
2020-04-26 23:47:47 -0400

    css toggle

--- src/components/ToggleSwitch.js
+++ src/components/ToggleSwitch.js
@@ -1,96 +1,62 @@
 import React from "react";
 
-import styled from "@emotion/styled";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 
-const Toggle = styled.div`
+const togSwitch = css`
   position: relative;
-  display: inline-block;
-  width: 75px;
-  vertical-align: middle;
-  text-align: left;
-  user-select: none;
+  display: block;
+  width: 8px;
+  height: 35px;
+  & input {
+    display: none;
   }
 `;
-
-const InnerSpan = styled.span`
-  display: block;
-  width: 200%;
-  margin-left: -100%;
-  transition: margin 0.3s ease-in 0s;
-    &:before{
-      content: "Yes";
-      text-transform: uppercase;
-      padding-left: 10px;
-      background-color: #f90;
-      color: #fff;
-  }
-  &:after{
-      content: "No";
-        text-transform: uppercase;
-        padding-right: 10px;
-        background-color: #ccc;
-        color: #fff;
-        text-align: right;
-  }
-  &:before, :after{
-      display: block;
-      float: left;
-      width: 50%;
-      height: 34px;
-      padding: 0;
-      line-height 34px;
-      font-size: 14px;
-      color: black;
-      font-weight: bold;
-      box-sizing: border-box;
-  }
-
-`;
-
-const ToggleSpan = styled.span`
-  display: block;
-  width: 24px;
-  margin: 5px;
+const slider = css`
+  cursor: pointer;
   position: absolute;
   top: 0;
+  left: 0;
+  right: 0;
   bottom: 0;
-  right: 40px;
-  border: 0 solid #ccc;
-  border-radius: 20px;
-  transition; all 0.3s ease-in 0s;
-  background: ${(props) => (props.disabled ? "#ccc" : "#fff")};
-  cursor: ${(props) => (props.disabled ? "not-allow" : "#default")};
-`;
-
-const SwitchCheckbox = styled.input`
-    display: none;
-
-    &:checked {
-      margin-left: 0;
-    }
-    &:check {
-      right: 0px;
-    }
+  background-color: white;
+  transition: 0.5s;
+  border: 3px solid gray;
+  &:before {
+    position: absolute;
+    content: "";
+    top: 2px;
+    left: 2px;
+    background-color: gray;
+    transition: 0.5s;
+  }
+  &:before {
+    width: 25px;
+    height: 25px;
+    border-radius: 100%;
   }
 `;
-
-const SwitchLabel = styled.label`
-  display: block;
-  overflow: hidden;
-  cursor: pointer;
-  border: 0 solid #ccc;
-  border-radius: 20px;
-  margin: 0;
+const round = css``;
+const togInput = css`
+  display: none;
+  & :checked {
+    transform: translateX(50px);
+    background: white;
+  }
 `;
 
 export const ToggleSwitch = (props) => {
   return (
-    <Toggle htmlFor="hrsTimeEntryToggle">
-      <SwitchCheckbox type="checkbox" name={props.Name} id={props.Name} />
-      <SwitchLabel htmlFor={props.Name}>
-        <InnerSpan />
-        <ToggleSpan />
-      </SwitchLabel>
-    </Toggle>
+    <div>
+      <label css={togSwitch} htmlFor={props.Name}>
+        <input
+          css={togInput}
+          type="checkbox"
+          name={props.Name}
+          id={props.Name}
+        />
+        <span css={[slider, round]}></span>
+      </label>
+    </div>
   );
 };
