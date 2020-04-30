9989773787450c5c6068073bcb3fa85c711fa9d2
MAtterholt <matterholt84@gmail.com>
2020-04-29 21:47:29 -0400

    add clear button

--- src/components/TaskBar.js
+++ src/components/TaskBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { UpdateDefaultTime } from "../appSections/UpdateDefaultTime";
 import { AddDayLog } from "../appSections/AddDayLog";
 import { GenerateWeekHr } from "../components/GenerateWeekHrs";
+import { ClearWeekHr } from "../components/ClearWeekHrs";
 
 import { TaskDiv } from "./style/TaskDiv";
 
@@ -9,6 +10,7 @@ export const TaskBar = () => {
   return (
     <TaskDiv>
       <GenerateWeekHr />
+      <ClearWeekHr />
       <AddDayLog />
       <UpdateDefaultTime />
     </TaskDiv>
