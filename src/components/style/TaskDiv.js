be2fe9216e4465e1088be083f16d667b29aa77a1
MAtterholt <matterholt84@gmail.com>
2020-10-15 10:37:05 -0400

    inital of refactor

--- src/components/style/TaskDiv.js
+++
@@ -1,5 +0,0 @@
-import styled from "@emotion/styled";
-
-export const TaskDiv = styled.div`
-  margin-top: 1px;
-`;
--- src/logic/hourConvert.js
+++
@@ -1,7 +0,0 @@
-export function twelveHrCovert(time) {
-  const [hour, min] = time.split(":");
-  const subtractHr = hour - 12;
-  const twelveHrFormat = `${subtractHr} : ${min} `;
-
-  return twelveHrFormat;
-}
