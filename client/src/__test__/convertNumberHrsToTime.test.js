098fe85f41b3a069c6765d50025d1b957449f0f2
MAtterholt <matterholt84@gmail.com>
2020-10-28 15:50:46 -0400

    convert input Number to useable time w/ test

--- client/src/__test__/convertNumberHrsToTime.test.js
+++ client/src/__test__/convertNumberHrsToTime.test.js
@@ -1,8 +1,8 @@
 import { convertNumberHrsToTime } from "../helpers/convertNumberHrsToTime";
 
 test("Convert the hours work to a workable time input", () => {
-  expect(convertNumberHrsToTime("8")).toBe("08.0");
-  expect(convertNumberHrsToTime("8.5")).toBe("08.5");
-  expect(convertNumberHrsToTime("10")).toBe("10.0");
-  expect(convertNumberHrsToTime("10.1")).toBe("10.1");
+  expect(convertNumberHrsToTime("8")).toStrictEqual(['08','0']);
+  expect(convertNumberHrsToTime("8.5")).toStrictEqual(['08','5']);
+  expect(convertNumberHrsToTime("10")).toStrictEqual(['10','0']);
+  expect(convertNumberHrsToTime("10.1")).toStrictEqual(['10','1']);
 });
--- client/src/helpers/convertNumberHrsToTime.js
+++ client/src/helpers/convertNumberHrsToTime.js
@@ -8,11 +8,11 @@ function convertNumberHrsToTime(hrsWorked) {
   // 8.1 convert to  08.1 but 10.1 convert to 10.1
   const [hrUnit, minUnit] = hrsWorked.split(".");
   
+  const fixedHour = hrUnit.length === 1 ? `0${hrUnit}` : `${hrUnit}`
   const fixedMins = minUnit === undefined ? "0" : minUnit;
-  const stringHr = hrUnit.length === 1 ? `0${hrUnit}` : `${hrUnit}`
   
 
-  return `${stringHr}.${fixedMins}`;
+  return [fixedHour, fixedMins];
 }
 
 export { convertNumberHrsToTime }
