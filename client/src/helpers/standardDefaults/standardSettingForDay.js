81f914c249edcaaf1d14de8523b57e7d6551d8ec
MAtterholt <matterholt84@gmail.com>
2020-12-21 11:39:10 -0500

    update hour data point

--- client/src/helpers/standardDefaults/standardSettingForDay.js
+++ client/src/helpers/standardDefaults/standardSettingForDay.js
@@ -1,11 +1,11 @@
 const standardSettingForDay = [
-  { id: 1, day: "Monday", isActive: true, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8" },
-  { id: 2, day: "Tuesday", isActive: true, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8"},
-  { id: 3, day: "Wednesday", isActive: true, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8"},
-  { id: 4, day: "Thursday", isActive: true, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8"},
-  { id: 5, day: "Friday", isActive: true, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8"},
-  { id: 6, day: "Saturday", isActive: false, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8"},
-  { id: 7, day: "Sunday", isActive: false, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8"},
+  { id: 1, day: "Monday", isActive: true, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8.0"},
+  { id: 2, day: "Tuesday", isActive: true, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8.0"},
+  { id: 3, day: "Wednesday", isActive: true, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8.0"},
+  { id: 4, day: "Thursday", isActive: true, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8.0"},
+  { id: 5, day: "Friday", isActive: true, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8.0"},
+  { id: 6, day: "Saturday", isActive: false, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8.0"},
+  { id: 7, day: "Sunday", isActive: false, punchIn: "08:00", punchOut: "16:00", hoursWorked:"8.0"},
 ];
 
 export { standardSettingForDay };
